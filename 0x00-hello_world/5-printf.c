ed1a4c736e96bf7790a1903a11090b94b0e5f962
Pepablue <shamaikadarlynne@gmail.com>
2023-03-20 02:19:31 -0700

    5-printf.c correction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line using printf function
- * Return: 0 (success)
+ * *Description:main - Prints out a string to stdout
+ * *Return: 0 (success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
-}	
