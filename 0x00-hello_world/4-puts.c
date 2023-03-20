328c7eae178398366237b0e583025f4d055fe074
Pepablue <shamaikadarlynne@gmail.com>
2023-03-20 01:20:50 -0700

    1 to 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main is the program that prints the puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
